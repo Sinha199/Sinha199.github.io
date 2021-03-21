6fc59483ec121644d30c746f16cb5e647dc446fd
Sinha199 <anshu.resource@gmail.com>
2021-03-21 16:52:24 +0530

    adding deploy files

---
+++ precache-manifest.f902ec828e976c47d96325c9d380c468.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73e2d1fdc6a5cc1b4138de57644a451",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3dc98b8566a8d17bf51a",
+    "url": "./static/css/main.137c0c20.chunk.css"
+  },
+  {
+    "revision": "f705a3ff6afefd55cc9b",
+    "url": "./static/js/2.88e45dff.chunk.js"
+  },
+  {
+    "revision": "3dc98b8566a8d17bf51a",
+    "url": "./static/js/main.b2a44390.chunk.js"
+  },
+  {
+    "revision": "04f9dce1e97d7eaf839f",
+    "url": "./static/js/runtime-main.c548fe5d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "./static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "./static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "./static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "./static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "./static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "./static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "./static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "199ad3c93cf438a31bc673b94eb85c7b",
+    "url": "./static/media/Kaggle_logo.199ad3c9.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "11557a6b5500ee87cbffa446f0bbc59f",
+    "url": "./static/media/Wecan-logo.11557a6b.jpeg"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "./static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "119de8c6667e8d46bade7417e7979604",
+    "url": "./static/media/app-logo.119de8c6.png"
+  },
+  {
+    "revision": "5da55eb13f50e4b27d0dbf92e432c498",
+    "url": "./static/media/asp-logo.5da55eb1.jpeg"
+  },
+  {
+    "revision": "3655563fff4063615689c01828d7dfc9",
+    "url": "./static/media/career_progress.3655563f.svg"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "./static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "61d7ee86fd56e257de563abe652bd58c",
+    "url": "./static/media/datascience.61d7ee86.svg"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "./static/media/deeplearning_ai_logo.7a9b2eb0.png"
+  },
+  {
+    "revision": "3667c86b8dbae0a30fc4cfc3baf9db61",
+    "url": "./static/media/ecell-logo.3667c86b.jpg"
+  },
+  {
+    "revision": "60c502a2573e763099a082f9a0f3dc01",
+    "url": "./static/media/experience.60c502a2.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "./static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "./static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "./static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "./static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "./static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "./static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "./static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "./static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "./static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "./static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "./static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "./static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "./static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "./static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "./static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "c11bd4ad9b4591be7b826d514a87f89b",
+    "url": "./static/media/familiar.c11bd4ad.svg"
+  },
+  {
+    "revision": "59239a6a006856ad1d590bbeed295499",
+    "url": "./static/media/fossee-logo.59239a6a.png"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "./static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "./static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "./static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "62fba16597fd6fb399b36a1602209310",
+    "url": "./static/media/graduation.62fba165.svg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "./static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "ea798a7abf761b1b7b965a9f16eee1a6",
+    "url": "./static/media/kubernetes.ea798a7a.png"
+  },
+  {
+    "revision": "faf83a7f1668abbf2652785c04ab16b4",
+    "url": "./static/media/nita.faf83a7f.png"
+  },
+  {
+    "revision": "5504b8112117e260221ae1f4226d68a9",
+    "url": "./static/media/noted.5504b811.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "./static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "0525906eade8caf3fe49f9bd33259997",
+    "url": "./static/media/projects_image.0525906e.svg"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "./static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "./static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "./static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "13078d3724fd881a36204970293c49d1",
+    "url": "./static/media/udemy_logo.13078d37.png"
+  },
+  {
+    "revision": "e9cd4c83b0c074a1ce326415a1a0b8a9",
+    "url": "./static/media/wwc-logo.e9cd4c83.jpg"
+  }
+]);
